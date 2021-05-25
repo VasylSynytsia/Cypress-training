8f3353e5fd0985d3c9cba63d72a23fba44f3929f
Myroslav Pasko <myroslav.pasko@arvosoftware.com>
2021-05-25 17:13:18 +0300

    actions

--- cypress/e2e/actions.spec.js
+++ cypress/e2e/actions.spec.js
@@ -3,8 +3,43 @@
 
 
 describe('Suite 1', ()=>{
-    it("first tests", function () {
+    it("Login", function () {
         cy.visit("cypress/index.html");
-        
     })
+
+    it("click", function() {
+        cy.get('[data-cy="milticlick"]').click({multiple: true})
+    })
+
+    it("type", function() {
+        cy.get('[data-cy="email"]').type('lol@email.com', {delay: 150})
+    })
+
+    it("select", function() {
+        cy.get('#courses').select('selenium1')
+    })
+
+    it("check", function() {
+        cy.get('#Banana').check()
+        cy.get('#Nuts').check()    
+        cy.get('#Nuts').uncheck()
+    })
+
+    it("clear inscription", function() {
+        cy.get('#inputPassword').type('123123123',{delay: 150})
+        cy.get('#inputPassword').clear()
+    })
+
+    it("right click + double click", function() {
+        cy.get('[data-cy="login"]').rightclick()
+        cy.get('.RememberMe').dblclick()
+    })
+
+    /*it("focus and blur", function() {
+        cy.get('.mb-3').focus()
+        cy.get('.mb-3').blur()
+    })*/
+
+    
+    
 })
