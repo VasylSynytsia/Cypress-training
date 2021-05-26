0f59c238cccd3fb04ed51007644f14fef948d46b
Myroslav Pasko <myroslav.pasko@arvosoftware.com>
2021-05-26 14:46:35 +0300

    123

--- cypress/e2e/assertions.spec.js
+++ cypress/e2e/assertions.spec.js
@@ -1,14 +1,18 @@
 ///<reference types="Cypress"/>
 
 describe('Suite 2', ()=>{
+
     it("Login", function () {
         cy.visit("cypress/index.html");
     })
 
-    it("test 1", function(){
-        
+    it("find by text", function(){
+        cy.get('body > :nth-child(6)').should('contain', 'list of')
+        cy.get('.course-list > .list2').should('have.text', 'Selenium')
     })
 
-
+    it("2", function(){
+        
+    })
 
 })
