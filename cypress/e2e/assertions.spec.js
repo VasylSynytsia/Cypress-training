cd9fff7ec62d2a966f333496b9bbf30210e6be5f
VasylSynytsia <vsinitsa1610@gmail.com>
2021-05-27 08:23:17 +0300

    morning 27.05

--- cypress/e2e/assertions.spec.js
+++ cypress/e2e/assertions.spec.js
@@ -7,12 +7,28 @@ describe('Suite 2', ()=>{
     })
 
     it("find by text", function(){
-        cy.get('body > :nth-child(6)').should('contain', 'list of')
+        cy.get('body > :nth-child(8)').should('contain', 'list of')
         cy.get('.course-list > .list2').should('have.text', 'Selenium')
     })
 
-    it("2", function(){
-        
+    it("visible", function(){
+        cy.get('.list9').should('have.text', 'react')
+        cy.get('.list9').should('not.be.visible')
     })
+
+    it("find by class", function(){
+        cy.contains('detox').should('have.class', 'mobile')
+    })
+
+    it("find by css", function(){
+        cy.get('.styles').should('have.css', 'background-color', 'rgb(255, 0, 0)')
+        cy.get('.styles').should('have.css', 'font-size').and('eql', '15.36px')
+    })
+
+    it("visible", function(){
+        cy.get('[data-cy=email]').should('be.visible')
+        cy.get('[data-cy=name]').should('be.disabled')
+    })
+
 
 })
