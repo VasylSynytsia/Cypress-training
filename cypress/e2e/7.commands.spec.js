9c5d6f6e4ed480ed5c6fe43ac0f3fa49b2722413
VasylSynytsia <vsinitsa1610@gmail.com>
2021-06-02 08:15:11 +0300

    start 'Commands' lesson

--- cypress/e2e/7.commands.spec.js
+++ cypress/e2e/7.commands.spec.js
@@ -6,6 +6,19 @@ describe('Suite 3', ()=>{
         cy.visit("cypress/index.html");
     })
 
+    it('URL', function(){
+        cy.url().should('contain', 'cypress/index')
+    })
+
+    it('Title', function(){
+        cy.title().should('eql', 'Cypress Tutorials test')
+    })
     
+    it('Go', function(){
+        cy.get('[data-cy=AddAttr]').click()
+        cy.go('back').url().should('contain', 'cypress')
+        cy.go('forward').url().should('contain', 'cypress')
+        cy.go('back')
+    })
 
 })
