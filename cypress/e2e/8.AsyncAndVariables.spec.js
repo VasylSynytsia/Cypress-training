748619a9cf3d7c1800e613d48d875304ebcb601b
VasylSynytsia <vsinitsa1610@gmail.com>
2021-06-25 08:50:12 +0300

    8 lesson

--- cypress/e2e/8.AsyncAndVariables.spec.js
+++ cypress/e2e/8.AsyncAndVariables.spec.js
@@ -1,1 +1,14 @@
 ///<reference types="Cypress"/>
+
+describe("Learning",() => {
+
+    it("1st lesson", function () {
+
+        let curentURL = ''
+        cy.visit('cypress/index.html')
+        cy.url().then(url => {
+            (curentURL = url)
+            expect(curentURL).to.contains('index.html')
+        })
+    })
+}) 
