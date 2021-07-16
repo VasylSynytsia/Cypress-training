1bfceb2734c0673096b3d7bfb95697f2b10bfa88
VasylSynytsia <vsinitsa1610@gmail.com>
2021-07-16 09:07:37 +0300

    test

--- cypress/e2e/9.fixtures.spec.js
+++ cypress/e2e/9.fixtures.spec.js
@@ -3,11 +3,28 @@
 describe("Suite 5",() => {
 
     it("first", function () {
-
-        cy.visit(cypress/index.html)
-        cy.fixture
+        cy.visit('cypress/index.html')
+        cy.fixture('user').then(user => {
+            cy.log(user.email)
+            cy.log(user.password)
+        })
     })
 
+    it("Paste text from fixtures", function () {
+        cy.fixture('user').then(user => {
+            cy.get('[data-cy=email]').type(user.email)
+            cy.get('#inputPassword').type(user.password)
+        })
+    })
 
+    beforeEach (() => {
+        cy.fixture('user').as('user')
+    })
+
+    it("Create varieble for test", function () {
+        cy.get('@user').then(user => {
+            cy.log(user)
+        })
+    })
 
 })
